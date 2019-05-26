c96841598d57729d3836edd0657063feae4fe63c
Thiru Moorthy <moorthy978@gmail.com>
2019-05-26 19:31:06 +0530

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,48 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include squid
+class squid2 {
+
+class {'squid': 
+  cache_mem                 => '512 MB',
+  workers                   => 3,
+  coredump_dir              => '/var/spool/squid',
+  http_access               => { 'boaz s3' => { action => 'allow', }},
+  http_ports                => { '10000' => { options => 'accel vhost', }},
+  snmp_ports                => { '1000' => { process_number => 3, }},
+  cache_dirs                => { '/data/' => { type => 'ufs', options => '15000 32 256 min-size=32769', process_number => 2 }},
+
+squid::acl {'boaz':
+  type    => 'src',
+  entries => ['100.96.0.0/11'],
+}
+
+squid::acl { 's3':
+  type    => 'dstdom_regex',
+  entries => ['.*s3\.amazonaws\.com',
+              '.*s3\.eu-central-1\.amazonaws\.com',
+              '.*s3\.sa-east-1\.amazonaws\.com',
+              '.*s3\.ap-northeast-1\.amazonaws\.com',
+              '.*s3\.eu-west-1\.amazonaws\.com',
+              '.*s3\.us-west-1\.amazonaws\.com',
+              '.*s3\.us-west-2\.amazonaws\.com',
+              '.*s3\.ap-southeast-2\.amazonaws\.com',
+              '.*s3\.ap-southeast-1\.amazonaws\.com',
+              '.*s3-eu-central-1\.amazonaws\.com',
+              '.*s3-sa-east-1\.amazonaws\.com',
+              '.*s3-ap-northeast-1\.amazonaws\.com',
+              '.*s3-eu-west-1\.amazonaws\.com',
+              '.*s3-us-west-1\.amazonaws\.com',
+              '.*s3-us-west-2\.amazonaws\.com',
+              '.*s3-ap-southeast-2\.amazonaws\.com',
+              '.*s3-ap-southeast-1\.amazonaws\.com',
+              ],
+}
+
+}
+
+}
+
