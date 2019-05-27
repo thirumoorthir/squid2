54015f91e7f5db304d3d547f91c851a4b8b40d0c
Thiru Moorthy <moorthy978@gmail.com>
2019-05-27 23:49:46 +0530

    puppet lint fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,24 +6,24 @@
 #   include squid
 class squid2 {
 
-class {'squid': 
-  cache_mem                 => '512 MB',
-  workers                   => 3,
-  config_user               => 'squid',
-  coredump_dir              => '/var/spool/squid',
-  http_access               => { 'boaz s3' => { action => 'allow', }},
-  http_ports                => { '10000' => { options => 'accel vhost', }},
-  snmp_ports                => { '1000' => { process_number => 3, }},
-  cache_dirs                => { '/data/' => { type => 'ufs', options => '15000 32 256 min-size=32769', process_number => 2 }},
+class {'squid':
+  cache_mem    => '512 MB',
+  workers      => 3,
+  config_user  => 'squid',
+  coredump_dir => '/var/spool/squid',
+  http_access  => { 'boaz s3' => { action => 'allow', }},
+  http_ports   => { '10000' => { options => 'accel vhost', }},
+  snmp_ports   => { '1000' => { process_number => 3, }},
+  cache_dirs   => { '/data/' => { type => 'ufs', options => '15000 32 256 min-size=32769', process_number => 2 }},
 }
- squid::acl {'boaz':
-  type    => 'src',
-  entries => ['100.96.0.0/11'],
+  squid::acl {'boaz':
+    type    => 'src',
+    entries => ['100.96.0.0/11'],
 }
 
- squid::acl { 's3':
-  type    => 'dstdom_regex',
-  entries => ['.*s3\.amazonaws\.com',
+  squid::acl { 's3':
+    type    => 'dstdom_regex',
+    entries => ['.*s3\.amazonaws\.com',
               '.*s3\.eu-central-1\.amazonaws\.com',
               '.*s3\.sa-east-1\.amazonaws\.com',
               '.*s3\.ap-northeast-1\.amazonaws\.com',
@@ -42,9 +42,7 @@ class {'squid':
               '.*s3-ap-southeast-1\.amazonaws\.com',
               ],
 }
-
-squid::http_port { '127.0.0.1:3128':
-}
+  squid::http_port { '127.0.0.1:3128':}
 
 
 }
